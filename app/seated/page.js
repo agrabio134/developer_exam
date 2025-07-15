f6c47f68717067e15ad4e8768ab3d85bd6076d72
agrabio134 <agrabioharvey@gmail.com>
2025-07-15 18:19:49 +0800

    initial commit

---
+++ app/seated/page.js
@@ -0,0 +1,9 @@
+
+export default async function SeatedPage() {
+  return (
+    <div className="container">
+      <h1 className="page-title">Seated Guests</h1>
+      <p>TODO: Add seated guests log</p>
+    </div>
+  );
+}
---
+++ app/waitlist/page.js
@@ -0,0 +1,8 @@
+export default async function WaitlistPage() {
+  return (
+    <div className="container">
+      <h1 className="page-title">Waitlist</h1>
+      <p>TODO: Add guest form and waitlist table</p>
+    </div>
+  );
+}
